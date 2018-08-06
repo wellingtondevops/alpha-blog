b96c2c54752cb8563ea09a1918f16d80d80f9825
wellingtodevops <wellingtondevops@gmail.com>
2018-08-06 11:25:04 +0000

    finalizado a criação de autenticação e controle de acoes dos usuários

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [:edit, :update, :show, :destroy]
+    before_action :require_user, except: [:index , :show]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
     
     def index
         @articles = Article.paginate(page:params[:page], per_page:5)
@@ -21,6 +23,7 @@ class ArticlesController < ApplicationController
        
         @article = Article.new(article_params)
         @article.user =  @currrent_user = User.find(session[:user_id]) 
+        
         #debugger
         if @article.save
             flash[:success]= "Article was successfully created"
@@ -72,6 +75,15 @@ class ArticlesController < ApplicationController
         
         def article_params
             params.require(:article).permit(:title, :description)
+        end
+        
+        def require_same_user
+            if current_user != @article.user
+                flash[:danger] = "You can only edit or delete your own articles"
+                redirect_to root_path
+            end
+            
+        
         end
     
 end
