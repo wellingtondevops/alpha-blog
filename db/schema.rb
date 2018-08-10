94b5f8c8d16c85a49f298b45ca175bde72c13481
wellingtodevops <wellingtondevops@gmail.com>
2018-08-10 00:41:46 +0000

    Create tests functionaly Add Category and test for class Category

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180806170454) do
+ActiveRecord::Schema.define(version: 20180808182127) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20180806170454) do
     t.datetime "create_at"
     t.datetime "update_at"
     t.integer  "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
